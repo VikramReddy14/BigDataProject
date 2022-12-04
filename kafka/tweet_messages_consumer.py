d99e286180e1836d97afc8b2420dc850080564ae
Sai Kishore Chowdam <saikishorechowdam18@gmail.com>
2022-12-04 16:05:29 -0600

    Updated with try-except block

--- kafka/tweet_messages_consumer.py
+++ kafka/tweet_messages_consumer.py
@@ -13,12 +13,16 @@ connection=mysql_conn.create_db_connection("localhost",my_sql_username,my_sql_pa
 consumer = KafkaConsumer('Twitter-Kafka',bootstrap_servers=['localhost:9092'],auto_offset_reset='latest')
 
 
-for message in consumer:
-    a= message.key
-    tweet_id = a.decode("utf-8")
-    b=message.value
-    tweet_value=b.decode("utf-8")
-    print(tweet_id)
-    print(tweet_value)
-    query1=f"Insert into tweet_messages values({tweet_id},'{tweet_value}')"
-    mysql_conn.execute_query(connection,query1)
+for message in consumer:    
+    try:
+            a= message.key
+            tweet_id = a.decode("utf-8")
+            b=message.value
+            tweet_value=b.decode("utf-8")
+            print(tweet_id)
+            print(tweet_value)
+            query1=f"Insert into tweet_messages values({tweet_id},'{tweet_value}')"
+            mysql_conn.execute_query(connection,query1)
+    except:
+            print("Error in data")
+            continue
